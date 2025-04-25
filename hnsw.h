7579f469d3f4934e86efd30fd1d44f5794602a78
steven <1085320540@qq.com>
2025-04-25 20:54:24 +0800

    rewrite: hnsw

--- hnsw.h
+++ hnsw.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <random>
-#include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 class HNSW {
 private:
@@ -17,15 +17,15 @@ private:
     int M;                                             // 新插入节点最大连接数
     int M_max;                                         // 每个节点的最大连接数
     int ef_construction;                               // 动态候选集大小
+    int q;                                             // 增长率
     std::vector<std::unordered_map<int, Node>> layers; // 各层的节点
     std::mt19937 rng;                                  // 随机数生成器
 
 public:
-    HNSW(int ml = 6, int m = 12, int ef = 60);
+    HNSW(int max_layers = 6, int M = 6, int M_max = 8, int ef_construction = 30, int q = 0.5);
 
     void insert(int id, const std::vector<float> &vec);
     std::vector<int> query(const std::vector<float> &q, int k);
-    void check_layers();
 
 private:
     int random_level();
